ff159118bdfeaeee699ecb24dc1492231b41fff4
kunal7238 <kunal.singh_cs19@gla.ac.in>
2022-03-04 19:34:28 +0530

    add some feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,12 +127,38 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
 });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
 
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop -50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // when the scroll is greater than 80 viewport height , add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top')
+    // when the scroll is heigher than 560 viewport height , add the scroll-scroll  class to the a tag with the scroll
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/ 
